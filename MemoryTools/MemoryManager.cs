391969ddd7473a400eec11c834b7471590d809bb
Александр Чугуевский <a1998ch@gmail.com>
2025-07-12 16:04:02 +1000

    feat: created library MemoryTools and CompactMemory method

---
+++ MemoryTools/MemoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryTools
+{
+    public class MemoryManager
+    {
+        public static void CompactMemory()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            var process = Process.GetCurrentProcess();
+            process.MinWorkingSet = process.MinWorkingSet;
+            process.MaxWorkingSet = process.MaxWorkingSet;
+        }
+    }
+}
