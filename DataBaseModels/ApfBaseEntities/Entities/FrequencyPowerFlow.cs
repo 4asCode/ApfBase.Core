3a00328deb510400e4d6392689ba8bde4c965058
Александр Чугуевский <a1998ch@gmail.com>
2025-07-22 06:35:56 +0300

    fix: updated Entities list

---
+++ DataBaseModels/ApfBaseEntities/Entities/FrequencyPowerFlow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseModels.ApfBaseEntities
+{
+    public partial class FrequencyPowerFlow : IEntity
+    {
+        public void Remove()
+        {
+            using (var context = new ApfBaseContext(
+                DataBaseConnection.ConnectionString))
+            {
+                var dbSet = context.Set<Seasons>();
+
+                var removeEntity = dbSet.Find(Id);
+
+                if (removeEntity != null)
+                {
+                    dbSet.Remove(removeEntity);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
