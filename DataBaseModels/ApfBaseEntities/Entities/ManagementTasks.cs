9bd6037f7a8bb0695147fab2486046f3fe16c011
Александр Чугуевский <a1998ch@gmail.com>
2025-07-26 17:53:40 +1000

    feat: added ReferenceDataEntityAttribute to ManagementTasks

---
+++ DataBaseModels/ApfBaseEntities/Entities/ManagementTasks.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataBaseModels.ApfBaseEntities.EntityAttribute;
+
+namespace DataBaseModels.ApfBaseEntities
+{
+    [ReferenceDataEntity]
+    public partial class ManagementTasks : IEntity
+    {
+        public void Remove()
+        {
+            using (var context = new ApfBaseContext(
+                DataBaseConnection.ConnectionString))
+            {
+                var dbSet = context.Set<Seasons>();
+
+                var removeEntity = dbSet.Find(Id);
+
+                if (removeEntity != null)
+                {
+                    dbSet.Remove(removeEntity);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
