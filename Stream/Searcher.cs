e74e916c169c113a4a6ebb232d1e1e3a8d719fd5
Александр Чугуевский <a1998ch@gmail.com>
2025-08-30 15:04:02 +1000

    feat(api): added Searcher method in Stream library

---
+++ Stream/Searcher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream
+{
+    public class Searcher
+    {
+        public static List<string> GetDirectories(
+            string path, string searchPattern = "*",
+            SearchOption searchOption = SearchOption.AllDirectories)
+        {
+            try
+            {
+                var directories = Directory.GetDirectories(
+                    path, searchPattern, searchOption).ToList();
+
+                if (searchOption == SearchOption.TopDirectoryOnly)
+                {
+                    return new List<string>() { path };
+                }
+                else if (searchOption == SearchOption.AllDirectories)
+                {
+                    directories.Add(path);
+                }
+
+                return directories;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return new List<string>();
+            }
+        }
+    }
+}
