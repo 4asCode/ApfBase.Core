65e3cc001dd8c31cd6ce40446e17d621395be84d
Александр Чугуевский <a1998ch@gmail.com>
2025-09-03 20:53:09 +1000

    feat(api): added the ability to save the list name in the Json format

--- ApfBuilder/PowerFlow/PowerFlowKind.cs
+++ ApfBuilder/PowerFlow/PowerFlowKind.cs
@@ -1,14 +1,22 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
 
 namespace ApfBuilder.PowerFlow
 {
     [JsonConverter(typeof(StringEnumConverter))]
     public enum PowerFlowKind
     {
+        [EnumMember(Value = "PowerFlowStandard")]
         PowerFlowStandard,
+
+        [EnumMember(Value = "PowerFlowSafe")]
         PowerFlowSafe,
+
+        [EnumMember(Value = "PowerFlowForcedState")]
         PowerFlowForcedState,
+
+        [EnumMember(Value = "PowerFlowEmergency")]
         PowerFlowEmergency
     }
 }
