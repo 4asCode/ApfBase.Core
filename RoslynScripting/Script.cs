5af3d7f9c343b2685b442c2a38a2443d4e6dbdb1
Александр Чугуевский <a1998ch@gmail.com>
2025-09-26 03:58:00 +0300

    refactor(api): minor change

--- RoslynScripting/Script.cs
+++ RoslynScripting/Script.cs
@@ -1,12 +1,10 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis.CSharp.Scripting;
-using Microsoft.CodeAnalysis.Scripting;
 
 namespace RoslynScripting
 {
